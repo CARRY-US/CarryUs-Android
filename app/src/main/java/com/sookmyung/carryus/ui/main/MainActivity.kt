75b96e6f870d865e4266c287280f0533f1389caf
Hailey <91793891+stellar-halo@users.noreply.github.com>
2024-02-16 18:07:55 +0900

    #1 mainActivity에 binding activity 적용

--- app/src/main/java/com/sookmyung/carryus/ui/main/MainActivity.kt
+++ app/src/main/java/com/sookmyung/carryus/ui/main/MainActivity.kt
@@ -1,12 +1,14 @@
 package com.sookmyung.carryus.ui.main
 
 import android.os.Bundle
-import androidx.appcompat.app.AppCompatActivity
 import com.sookmyung.carryus.R
+import com.sookmyung.carryus.databinding.ActivityMainBinding
+import com.sookmyung.carryus.util.binding.BindingActivity
+import dagger.hilt.android.AndroidEntryPoint
 
-class MainActivity : AppCompatActivity() {
+@AndroidEntryPoint
+class MainActivity : BindingActivity<ActivityMainBinding>(R.layout.activity_main) {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
     }
 }
