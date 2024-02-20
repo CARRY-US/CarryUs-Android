5dd4a41b7d57b1805d4cbe34f2812c9d0472ec3b
skfl0282 <90668082+skfl0282@users.noreply.github.com>
2024-02-20 15:04:50 +0900

    #4 말줄임표 추가

--- app/src/main/java/com/sookmyung/carryus/ui/reservationlist/ReservationShopAdapter.kt
+++ app/src/main/java/com/sookmyung/carryus/ui/reservationlist/ReservationShopAdapter.kt
@@ -14,8 +14,21 @@ class ReservationShopAdapter(var reservationList: List<ReservationList>):
 
     inner class viewHolder(private val itemBinding: ItemShopReservationListBinding): RecyclerView.ViewHolder(itemBinding.root){
         fun bind(data:ReservationList) {
-            itemBinding.tvShopName.text = data.shopName
-            itemBinding.tvShopLocation.text = data.shopLocation
+
+            val truncatedShopName = if (data.shopName.length > 14) {
+                "${data.shopName.substring(0, 14)} ..."
+            } else {
+                data.shopName
+            }
+
+            val truncatedShopLocation = if (data.shopLocation.length > 18) {
+                "${data.shopLocation.substring(0, 14)} ..."
+            } else {
+                data.shopLocation
+            }
+
+            itemBinding.tvShopName.text = truncatedShopName
+            itemBinding.tvShopLocation.text = truncatedShopLocation
             itemBinding.tvReservationDate.text = data.reservationDate
             itemBinding.ivShopIcon.setImageResource(R.drawable.ic_shop_img)
         }
