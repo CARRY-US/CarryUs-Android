b0c611ec3f496e00374137b6dd2e1a3c4cbb83b8
skfl0282 <90668082+skfl0282@users.noreply.github.com>
2024-02-20 13:25:41 +0900

    #4 예약기록 ViewModel 추가 + tablayout 설정

---
+++ app/src/main/java/com/sookmyung/carryus/ui/reservationlist/ReservationListViewModel.kt
@@ -0,0 +1,26 @@
+package com.sookmyung.carryus.ui.reservationlist
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.viewpager.widget.PagerAdapter
+
+class ReservationListViewModel : ViewModel(){
+    val tabItems: LiveData<List<String>> get() = _tabItems
+    val position: LiveData<Int> get() = _position
+
+    private val _tabItems: MutableLiveData<List<String>> = MutableLiveData()
+    private val _position: MutableLiveData<Int> = MutableLiveData()
+
+    companion object {
+        private val TAB_ITEMS = listOf("수락", "대기", "완료", "취소")
+    }
+
+    init {
+        _tabItems.postValue(TAB_ITEMS)
+    }
+
+    fun selectPosition(position: Int) {
+        _position.postValue(position)
+    }
+}
