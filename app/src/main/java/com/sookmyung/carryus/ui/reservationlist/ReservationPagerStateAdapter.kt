3a77ba0408272d8409d684340cac9ac6728fd23c
skfl0282 <90668082+skfl0282@users.noreply.github.com>
2024-02-20 13:54:07 +0900

    #4 탭 분기 처리

--- app/src/main/java/com/sookmyung/carryus/ui/reservationlist/ReservationPagerStateAdapter.kt
+++ app/src/main/java/com/sookmyung/carryus/ui/reservationlist/ReservationPagerStateAdapter.kt
@@ -1,8 +1,10 @@
 package com.sookmyung.carryus.ui.reservationlist
 
+import android.os.Bundle
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentActivity
 import androidx.viewpager2.adapter.FragmentStateAdapter
+import com.sookmyung.carryus.domain.entity.ReservationStatus
 
 class ReservationPagerStateAdapter (fragmentActivity: FragmentActivity): FragmentStateAdapter(fragmentActivity) {
 
@@ -13,6 +15,17 @@ class ReservationPagerStateAdapter (fragmentActivity: FragmentActivity): Fragmen
 
     // FragmentStateAdapter 상속 시 무조건 override 해야하는 fun (View Pager의 position에 해당하는 fragment return)
     override fun createFragment(position: Int): Fragment {
-        return ReservationPagerFragment()
+        val fragment = ReservationPagerFragment()
+
+        val arguments = Bundle()
+        when (position) {
+            0 -> arguments.putString("content", ReservationStatus.ACCEPTED.name)
+            1 -> arguments.putString("content", ReservationStatus.WAITING.name)
+            2 -> arguments.putString("content", ReservationStatus.COMPLETED.name)
+            3 -> arguments.putString("content", ReservationStatus.CANCELED.name)
+        }
+
+        fragment.arguments = arguments
+        return fragment
     }
 }
