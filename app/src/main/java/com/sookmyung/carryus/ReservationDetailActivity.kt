fbca07ed5b2de4b47b0630173f2d86b84b8c9e22
skfl0282 <90668082+skfl0282@users.noreply.github.com>
2024-02-29 17:31:08 +0900

    #8 예약 상세 activity 추가

---
+++ app/src/main/java/com/sookmyung/carryus/ReservationDetailActivity.kt
@@ -0,0 +1,11 @@
+package com.sookmyung.carryus
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ReservationDetailActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_reservation_detail)
+    }
+}
