11bb696c32a8221f07efc1e7716aa3d52298eb34
skfl0282 <90668082+skfl0282@users.noreply.github.com>
2024-03-16 14:29:32 +0900

    #8 예약 상세 dto

---
+++ app/src/main/java/com/sookmyung/carryus/domain/entity/ReservationDetailResponse.kt
@@ -0,0 +1,15 @@
+package com.sookmyung.carryus.domain.entity
+
+data class ReservationDetailResponse(
+    val reservationId: Int,
+    val storeId: Int,
+    val storeImgUrl: String,
+    val storeName: String,
+    val reservationType: String,
+    val reservationDate: String,
+    val reservationBaggageInfo: String,
+    val memberName: String,
+    val memberPhoneNumber: String,
+    val reservationRequest: String,
+    val reservationPayment: Int
+)
