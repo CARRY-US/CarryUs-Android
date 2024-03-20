b00d1bea721ab76ddb71b1530a959dd3da33e003
skfl0282 <90668082+skfl0282@users.noreply.github.com>
2024-03-20 16:09:08 +0900

    #12 후기 수정 xml

--- app/src/main/java/com/sookmyung/carryus/ui/review/ReviewInquiryActivity.kt
+++ app/src/main/java/com/sookmyung/carryus/ui/review/ReviewInquiryActivity.kt
@@ -1,12 +1,19 @@
 package com.sookmyung.carryus.ui.review
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
 import com.sookmyung.carryus.R
+import com.sookmyung.carryus.ReviewEditActivity
+import com.sookmyung.carryus.databinding.ActivityReviewInquiryBinding
+import com.sookmyung.carryus.util.binding.BindingActivity
 
-class ReviewInquiryActivity : AppCompatActivity() {
+class ReviewInquiryActivity : BindingActivity<ActivityReviewInquiryBinding>(R.layout.activity_review_inquiry)
+{
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_review_inquiry)
+        binding.tvReviewInquiryEdit.setOnClickListener {
+            val intent = Intent(this, ReviewEditActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
