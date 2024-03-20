09a25da0e89678273522b7bcff0d4fe1bca75660
skfl0282 <90668082+skfl0282@users.noreply.github.com>
2024-03-20 23:59:17 +0900

    #12 후기 수정 viewmodel

--- app/src/main/java/com/sookmyung/carryus/ui/review/ReviewEditViewModel.kt
+++ app/src/main/java/com/sookmyung/carryus/ui/review/ReviewEditViewModel.kt
@@ -2,7 +2,9 @@ package com.sookmyung.carryus.ui.review
 
 import android.text.Editable
 import android.text.TextWatcher
+import android.util.Log
 import android.widget.EditText
+import android.widget.RatingBar
 import android.widget.TextView
 import androidx.databinding.BindingAdapter
 import androidx.lifecycle.LiveData
@@ -14,6 +16,12 @@ import com.sookmyung.carryus.domain.entity.ReviewDetail
 class ReviewEditViewModel : ViewModel(){
     private val _textCount = MutableLiveData<String>()
     val textCount: LiveData<String> = _textCount
+
+    private val _rating = MutableLiveData<Float>()
+    val rating: LiveData<Float> = _rating
+
+    private val _reviewContent = MutableLiveData<String>()
+    val reviewContent: LiveData<String> = _reviewContent
 
     private val _reviewDetailLiveData = MutableLiveData<ReviewDetail>()
     val reviewDetailLiveData: LiveData<ReviewDetail> = _reviewDetailLiveData
@@ -31,22 +39,35 @@ class ReviewEditViewModel : ViewModel(){
 
     fun onTextChanged(s: CharSequence) {
         val count = s.length
+        _reviewContent.value = s.toString()
         _textCount.value = "$count/$MAXIMUM_LENGTH"
     }
 
-    fun initializeTextCount(reviewDetail: ReviewDetail) {
+    fun onRatingChanged(rating: Float) {
+        _rating.value = rating
+        Log.d("ReviewEditViewModel", "rating: $rating")
+    }
+
+    fun initializeDataSet(reviewDetail: ReviewDetail) {
         val count = reviewDetail.reviewContent.length
         _textCount.value = "$count/$MAXIMUM_LENGTH"
+        _rating.value = reviewDetail.reviewRating
+        _reviewContent.value = reviewDetail.reviewContent
     }
 
     fun setReviewDetail(reviewDetail: ReviewDetail) {
         _reviewDetailLiveData.value = reviewDetail
-        initializeTextCount(reviewDetail)
-
+        initializeDataSet(reviewDetail)
     }
 
     fun setReservationList(reservationList: ReservationList) {
         _reservationListLiveData.value = reservationList
+    }
+
+    fun onSaveButtonClicked() {
+        Log.d("ReviewEditViewModel", "onSaveButtonClick")
+        Log.d("ReviewEditViewModel", "rating: ${_rating.value}")
+        Log.d("ReviewEditViewModel", "reviewContent: ${_reviewContent.value}")
     }
 }
 
@@ -65,5 +86,14 @@ fun bindTextWatcher(editText: EditText, viewModel: ReviewEditViewModel?) {
             override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
             override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {}
         })
+    }
+}
+
+@BindingAdapter("app:ratingChangeListener")
+fun setRatingChangeListener(ratingBar: RatingBar, viewModel: ReviewEditViewModel?) {
+    viewModel?.let {
+        ratingBar.setOnRatingBarChangeListener { _, rating, _ ->
+            viewModel.onRatingChanged(rating)
+        }
     }
 }
