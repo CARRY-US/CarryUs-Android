924a8417c01dcce1dd22ab8fe8fb82812ec95efa
Hailey <91793891+stellar-halo@users.noreply.github.com>
2024-03-22 17:19:41 +0900

    #25 sample 파일들 삭제

--- app/src/main/java/com/sookmyung/carryus/domain/repository/SampleRepository.kt
+++
@@ -1,7 +0,0 @@
-package com.sookmyung.carryus.domain.repository
-
-import com.sookmyung.carryus.domain.entity.Sample
-
-interface SampleRepository {
-    suspend fun sample(name: String): Result<Sample>
-}
